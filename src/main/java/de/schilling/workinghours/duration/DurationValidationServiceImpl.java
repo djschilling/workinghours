cb8100621761d054fff85908af92d510e4cfa48b
Raphael <e-mail@fak.de>
2015-08-12 17:47:26 +0200

    This Closes #5

--- src/main/java/de/schilling/workinghours/duration/DurationValidationServiceImpl.java
+++ src/main/java/de/schilling/workinghours/duration/DurationValidationServiceImpl.java
@@ -21,30 +21,35 @@ public class DurationValidationServiceImpl implements DurationValidationService 
 
     @Override
     public boolean validateNewDuration(Duration duration) {
-
+        if(duration.getEndTime() != null && duration.getDuration() == 0) {
+            return false;
+        }
+        if(duration.getStartTime() == null) {
+            return false;
+        }
+        boolean foundInvalidCombination = false;
         List<Duration> durationList = durationRepository.findByUsernameOrderByStartTimeDesc(duration.getUsername());
         for (Duration currentDuration : durationList) {
             if (currentDuration.getEndTime() == null) {
                 if (!isAfterFirstOrEqual(currentDuration.getStartTime(), duration.getEndTime()) &&
                         !currentDuration.getStartTime().isBefore(duration.getStartTime())) {
-                    return false;
+                    foundInvalidCombination = true;
                 }
             } else {
                 if (duration.getEndTime() == null) {
                     if (duration.getStartTime().isBefore(currentDuration.getEndTime()) &&
                             !duration.getStartTime().isBefore(currentDuration.getStartTime())) {
-                        return false;
+                        foundInvalidCombination = true;
                     }
                 } else {
                     if (!isAfterFirstOrEqual(currentDuration.getEndTime(), duration.getStartTime()) &&
                             isAfterFirstOrEqual(currentDuration.getStartTime(), duration.getEndTime())) {
-                        return false;
+                        foundInvalidCombination = true;
                     }
                 }
             }
         }
-
-        return true;
+        return !foundInvalidCombination;
     }
 
     private boolean isAfterFirstOrEqual(LocalDateTime first, LocalDateTime second) {
